f609bc939cc41ac924cdb64eefcbd6674b7d76cd
SabinShrestha133 <stha.sabin133@gmail.com>
2025-07-24 20:54:30 +0545

    added pages

---
+++ app/src/main/java/com/example/bikeshop/view/SplashActivity.kt
@@ -0,0 +1,69 @@
+package com.example.bikeshop.view
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.sportsequipmentstore.LoginActivity
+import com.example.sportsequipmentstore.R
+import kotlinx.coroutines.delay
+import kotlin.jvm.java
+
+class SplashActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SplashBody()
+        }
+    }
+}
+
+@Composable
+fun SplashBody() {
+    val context = LocalContext.current
+    val activity = context as Activity
+
+    val sharedPreferences = context.getSharedPreferences("User", Context.MODE_PRIVATE)
+    val localEmail = sharedPreferences.getString("email", "").toString()
+
+    LaunchedEffect(Unit) {
+        delay(3000) // Wait for 3 seconds
+        val intent = Intent(context, LoginActivity::class.java)
+        context.startActivity(intent)
+        activity.finish()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.retrocruglogo),
+            contentDescription = "RetroCrug Logo",
+            modifier = Modifier
+                .width(300.dp)
+                .height(300.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewSplash() {
+    SplashBody()
+}
