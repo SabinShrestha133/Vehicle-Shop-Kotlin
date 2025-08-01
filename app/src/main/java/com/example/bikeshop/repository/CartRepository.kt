a214906aaff908b5217b375a687a1882e310bb4b
SabinShrestha133 <stha.sabin133@gmail.com>
2025-08-01 14:14:23 +0545

    Updated

---
+++ app/src/main/java/com/example/bikeshop/repository/CartRepository.kt
@@ -0,0 +1,10 @@
+package com.example.bikeshop.repository
+
+import com.example.bikeshop.model.CartItemModel
+
+interface CartRepository {
+    fun addToCart(item: CartItemModel, callback: (Boolean, String) -> Unit)
+    fun getCartItems(callback: (List<CartItemModel>, Boolean, String) -> Unit)
+    fun updateCartItemQuantity(cartItemId: String, quantity: Int, callback: (Boolean, String) -> Unit)
+    fun removeCartItem(cartItemId: String, callback: (Boolean, String) -> Unit)
+}
